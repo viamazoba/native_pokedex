94e556b7e435fbfb79c204f13fd8dda9fce46a54
Victor Mazo <viamazoba@unal.edu.co>
2025-03-24 12:49:24 -0500

    feat: Se implementa infinite scroll en la cards implementadas en el home de la aplicación

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { PakeballBackground } from '../../components/ui/PokeballBackground';
 import { FlatList } from 'react-native-gesture-handler';
 import { globalTheme } from '../../../config/theme/global-theme';
@@ -13,9 +13,18 @@ export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets();
 
-    const { isLoading, data: pokemons = [] } = useQuery({
+    // Est es la forma tradicional de una petición http
+    /* const { isLoading, data: pokemons = [] } = useQuery({
         queryKey: ['pokemons'],
         queryFn: () => getPokemons(0),
+        staleTime: 1000 * 60 * 60, // Tiempo que se va a guardar en caché la información
+    }); */
+
+    const { isLoading, data, fetchNextPage } = useInfiniteQuery({
+        queryKey: ['pokemons', 'infinite'],
+        initialPageParam: 0,
+        queryFn: (params) => getPokemons(params.pageParam),
+        getNextPageParam: (lastPage, pages) => pages.length,
         staleTime: 1000 * 60 * 60, // Tiempo que se va a guardar en caché la información
     });
 
@@ -24,7 +33,7 @@ export const HomeScreen = () => {
             <PakeballBackground style={styles.imgPosition} />
 
             <FlatList
-                data={pokemons}
+                data={data?.pages.flat() ?? []}
                 keyExtractor={(pokemon, index) => `${pokemon.id}--${index}`}
                 numColumns={2}
                 ListHeaderComponent={() => (
@@ -34,6 +43,9 @@ export const HomeScreen = () => {
                 renderItem={({ item }) => (
                     <PokemonCard pokemon={item} />
                 )}
+                onEndReachedThreshold={0.6}
+                onEndReached={() => fetchNextPage()}
+                showsVerticalScrollIndicator={false}
             />
 
         </View>
