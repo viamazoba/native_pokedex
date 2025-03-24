ad94c4e600ce77c9457bb88e371ce7b2dec7c7e2
Victor Mazo <viamazoba@unal.edu.co>
2025-03-24 11:57:50 -0500

    feat: Se implementan imágenes con fade in para cards del home

--- src/presentation/components/pokemons/PokemonCard.tsx
+++ src/presentation/components/pokemons/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import { View, StyleSheet, Image } from 'react-native';
 import { Pokemon } from '../../../domain/entities/pokemon';
 import { Card, Text } from 'react-native-paper';
+import { FadeInImage } from '../ui/FadeInImage';
 
 
 interface Props {
@@ -28,8 +29,8 @@ export const PokemonCard = ({
                 />
             </View>
 
-            <Image
-                source={{ uri: pokemon.avatar }}
+            <FadeInImage
+                uri={pokemon.avatar}
                 style={styles.pokemonImage}
             />
 
