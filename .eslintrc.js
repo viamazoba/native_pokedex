3febc5c0022c04d34c7d7dd16d0b65b85b33912e
Victor Mazo <viamazoba@unal.edu.co>
2025-03-23 19:33:09 -0500

    feat: Se realiza segunda versión del home de la aplicación

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
   rules: {
     'curly': 'off',
     'react-native/no-inline-styles': 'off',
+    'react/no-unstable-nested-components': 'off',
   },
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -3,19 +3,38 @@ import { Button, Text } from 'react-native-paper';
 import { getPokemons } from '../../../actions/pokemons';
 import { useQuery } from '@tanstack/react-query';
 import { PakeballBackground } from '../../components/ui/PokeballBackground';
+import { FlatList } from 'react-native-gesture-handler';
+import { globalTheme } from '../../../config/theme/global-theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../../components/pokemons/PokemonCard';
 
 
 export const HomeScreen = () => {
 
-    const { isLoading, data = [] } = useQuery({
+    const { top } = useSafeAreaInsets();
+
+    const { isLoading, data: pokemons = [] } = useQuery({
         queryKey: ['pokemons'],
         queryFn: () => getPokemons(0),
         staleTime: 1000 * 60 * 60, // Tiempo que se va a guardar en caché la información
     });
 
     return (
-        <View>
+        <View style={globalTheme.globalMargin}>
             <PakeballBackground style={styles.imgPosition} />
+
+            <FlatList
+                data={pokemons}
+                keyExtractor={(pokemon, index) => `${pokemon.id}--${index}`}
+                numColumns={2}
+                ListHeaderComponent={() => (
+                    <Text variant="displayMedium">Pokédex</Text>
+                )}
+                style={{ paddingTop: top + 20 }}
+                renderItem={({ item }) => (
+                    <PokemonCard pokemon={item} />
+                )}
+            />
 
         </View>
     );
