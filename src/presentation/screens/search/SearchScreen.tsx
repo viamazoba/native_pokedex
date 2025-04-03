b794091472259f9e9dda332428e276527303dff5
Victor Mazo <viamazoba@unal.edu.co>
2025-04-03 07:36:17 -0500

    feat: Se finaliza funcionalidad del buscador

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,12 +1,12 @@
 import { View, FlatList } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { globalTheme } from '../../../config/theme/global-theme';
-import { ActivityIndicator, Text, TextInput } from 'react-native-paper';
-import { Pokemon } from '../../../domain/entities/pokemon';
+import { ActivityIndicator, TextInput } from 'react-native-paper';
 import { PokemonCard } from '../../components/pokemons/PokemonCard';
 import { useQuery } from '@tanstack/react-query';
-import { getPokemonNameWithID } from '../../../actions/pokemons';
+import { getPokemonNameWithID, getPokemonsByIds } from '../../../actions/pokemons';
 import { useMemo, useState } from 'react';
+import { FullScreenLoader } from '../../components/ui/FullScreenLoader';
 
 
 export const SearchScreen = () => {
@@ -33,6 +33,16 @@ export const SearchScreen = () => {
 
     }, [term, pokemonNameList]);
 
+    const { isLoading: isLoadingPokemons, data: pokemons = [] } = useQuery({
+        queryKey: ['pokemons', 'by', pokemonNameIdList],
+        queryFn: () => getPokemonsByIds(pokemonNameIdList.map(pokemon => pokemon.id)),
+        staleTime: 1000 * 60 * 5,
+    });
+
+    if (isLoading) {
+        return (<FullScreenLoader />);
+    }
+
     return (
         <View style={[globalTheme.globalMargin, { paddingTop: top + 10 }]}>
             <TextInput
@@ -44,12 +54,16 @@ export const SearchScreen = () => {
                 value={term}
             />
 
-            <ActivityIndicator
-                style={{ paddingTop: 20 }}
-            />
+            {
+                isLoadingPokemons &&
+                <ActivityIndicator
+                    style={{ paddingTop: 20 }}
+                />
+            }
+
 
             <FlatList
-                data={[] as Pokemon[]}
+                data={pokemons}
                 keyExtractor={(pokemon, index) => `${pokemon.id}--${index}`}
                 numColumns={2}
                 style={{ paddingTop: top + 20 }}
@@ -57,6 +71,7 @@ export const SearchScreen = () => {
                     <PokemonCard pokemon={item} />
                 )}
                 showsVerticalScrollIndicator={false}
+                ListFooterComponent={<View style={{ height: 120 }} />}
             />
         </View>
     );
